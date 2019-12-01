88f3ac09f9787bf221880e74dd614cc16a464040
Josiah Bradbury <herrozerro@gmail.com>
2019-12-01 10:49:24 -0600

    Add project files.

---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fuel1 = 0;
+
+            var f = DayOne.GetAllFuel(100756);
+
+            foreach (var component in DayOne.Inputs)
+            {
+                fuel1 += DayOne.GetAllFuel(component);
+            }
+
+            Console.WriteLine($"First Method Sum: {fuel1}");
+        }
+    }
+}
