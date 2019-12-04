fbf316d742f79150c5ae950c46b511c1ab07b5b1
Josiah Bradbury <herrozerro@gmail.com>
2019-12-04 13:50:43 -0600

    refactor

--- AdventOfCode2019/Completed/Day02.cs
+++ AdventOfCode2019/Completed/Day02.cs
@@ -7,7 +7,23 @@ namespace AdventOfCode2019
 {
     public static class Day02
     {
-        public static void RunAllComobos()
+        public static void RunDay()
+        {
+            Console.WriteLine("Day 2");
+            
+            var lines = Program.GetStringFromFile("Day2.txt");
+
+            var output = mysterfunc(12,2, lines);
+            Console.WriteLine(output);
+            
+            var outputstr = RunAllComobos(lines);
+            Console.WriteLine(outputstr);
+            
+            Console.WriteLine("**************");
+            Console.WriteLine(Environment.NewLine);
+        }
+        
+        public static string RunAllComobos(string strcode)
         {
             bool isfound = true;
             while (isfound)
@@ -16,32 +32,22 @@ namespace AdventOfCode2019
                 {
                     for (int j = 0;  j < 99;  j++)
                     {
-                        code = (strcode ?? "").Split(',').Select<string, int>(int.Parse).ToArray();
-                        var fun = mysterfunc(i, j);
+                        var fun = mysterfunc(i, j, strcode);
                         isfound = 19690720 != fun;
                         Console.WriteLine($"x{i} y{j} value = {fun}");
                         if (isfound == false)
                         {
                             Console.WriteLine($"FOUND! x{i} y{j}");
-
+                            return $"{i}{j}";
                         }
                     }
                 }
             }
         }
 
-        public static void RunDay()
+        public static int mysterfunc(int i, int j, string strcode)
         {
-            Console.WriteLine("Day 2");
-            
-            Console.WriteLine(string.Concat(code),',');
-            Console.WriteLine("**************");
-            Console.WriteLine(Environment.NewLine);
-        }
-
-
-        public static int mysterfunc(int i, int j)
-        {
+            code = (strcode ?? "").Split(',').Select<string, int>(int.Parse).ToArray();
             CurrentPosition = 0;
             bool isrunning = true;
             int x = 0;
@@ -68,10 +74,8 @@ namespace AdventOfCode2019
 
             return code[0];
         }
-        static string strcode = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0";
-        //public static int[] code = new int[] { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 };
-        static int[] code;
-
+        
+        public static int[] code;
         public static int CurrentPosition = 0;
 
         public static void Op1()
@@ -91,35 +95,5 @@ namespace AdventOfCode2019
             code[xstorepos] = code[x1pos] * code[x2pos];
             CurrentPosition += 4;
         }
-
-        //public static bool MysteryFunc(int x, int y)
-        //{
-        //    var x2 = 19690720;
-        //    var codecopy = (int[])code.Clone();
-        //    codecopy[1] = x;
-        //    codecopy[2] = y;
-        //    bool isrunning = true;
-        //    int pos = 0;
-        //    while (isrunning)
-        //    {
-        //        pos = code[CurrentPosition];
-
-        //        switch (pos)
-        //        {
-        //            case 1:
-        //                Op1();
-        //                break;
-        //            case 2:
-        //                Op2();
-        //                break;
-        //            case 99:
-        //            default:
-        //                isrunning = false;
-        //                break;
-        //        }
-        //    }
-
-        //    return x2 == code[0];
-        //}
     }
 }
